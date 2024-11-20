cd2d2eade18a6e81991682f39b50b2a8913ec715
Zaid Y <zyazadi2000@gmail.com>
2024-11-19 18:48:22 -0800

    Added outbound rules

--- main.tf
+++ main.tf
@@ -39,6 +39,13 @@ resource "aws_security_group" "web_access" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
 output "instance_id" {
